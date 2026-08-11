5407f6eb89e66942ca24bd5fe37f15af1fd79324
kedarshrish <kedarshrish@gmail.com>
2026-08-12 01:22:30 +0530

    Added 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+
+int age;
+printf("Enetr Age :");
+scanf("%d",&age);
+
+age >= 18 ? printf("Adult") : printf("Not Adult"); // Ternary operator ?  // condtion ? print if ture : print if false ;
+
+return 0;
+
+
+
+}
