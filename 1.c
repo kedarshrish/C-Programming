220de0583a8e67e27b413789a14beff6b1c907c4
kedarshrish <kedarshrish@gmail.com>
2026-08-12 01:02:27 +0530

    Addded 1.c

---
+++ 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main (){
+
+int a,b;
+int sum;
+
+printf("enter value of a a");
+scanf("%d",&a);
+
+printf("enter value of a b");
+scanf("%d",&b);
+
+sum = a + b ;
+
+printf("%d",sum);
+
+return 0;
+
+
+
+
+
+
+
+
+}
