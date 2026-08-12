1425d3c850684de5026862173167ecba72128dc4
kedarshrish <kedarshrish@gmail.com>
2026-08-13 00:08:13 +0530

    Added 8.c

---
+++ 8.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+
+int marks;
+printf("Enter marks : ");
+scanf("%d",&marks);
+
+if(marks < 30 && marks >= 0 ){
+printf("C");
+}
+
+else if (marks >= 30 && marks < 70 ){
+
+printf("B");
+}
+
+else if (marks >= 70 && marks < 90 ){
+
+printf("A");
+}
+
+else if (marks >= 90 && marks <= 100 ){
+
+printf("A+");
+}
+
+else{
+
+printf("Tnvalid marks entered ");
+}
+
+return 0;
+
+}
---
+++ 9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+int x = 2;
+
+if (x = 0){
+    
+    printf("x is 1 ");
+
+}
+
+else{
+
+printf("x is not 1 ");
+
+}
+return 0;
+}
