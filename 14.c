7da575dc31c4ec7253c5f140b830f44f51b73f91
kedarshrish <kedarshrish@gmail.com>
2026-08-14 13:39:27 +0530

    Added 14.c

---
+++ 14.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+
+int i = 0;
+int n;
+printf("Enter value of n ");
+scanf("%d",&n);
+ 
+while(i<=n){
+
+    printf("%d \n", i );
+    i++;
+}
+
+return 0;
+
+}
+
+// #include<stdio.h>
+
+// int main(){
+
+// int n;
+// printf("Enter value of n ");
+// scanf("%d",&n);
+
+// for(int i = 0 ; i <=n ; i++){
+
+//     printf("%d \n",i);
+// }
+
+// return 0;
+// }
