f2da9199cd7a808ba6907c83c241ee6e93822cf9
kedarshrish <kedarshrish@gmail.com>
2026-08-14 13:38:37 +0530

    Added 13.c

---
+++ 13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+
+for (float i = 1.0 ; i <= 5.0 ; i++){
+
+    printf("%f \n", i );
+   
+}
+
+printf("\n");
+
+for (char ch = 'a'; ch <= 'z'; ch++){
+
+    printf("%c \n",ch);
+}
+
+return 0;
+}
