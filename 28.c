16cbc76928cddaf7e26c39321d2cbfbbd26addae
kedarshrish <kedarshrish@gmail.com>
2026-08-17 16:13:22 +0530

    Added 28.c

---
+++ 28.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+// declare
+void printHello(); 
+
+
+int main(){
+// call
+    printHello();
+    
+    printHello();
+    
+    printHello();
+    
+    printHello();
+
+    printHello();
+    return 0;
+    
+}
+// definition
+ void printHello(){
+
+    printf("Hello\n");
+    printf("My name is Shrish \n");
+}
