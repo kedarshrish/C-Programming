f42ead30982729e845233e71a53987b712b94bed
kedarshrish <kedarshrish@gmail.com>
2026-08-17 16:16:03 +0530

    Added 30.c

---
+++ 30.c
@@ -0,0 +1,76 @@
+// #include<stdio.h>
+
+
+
+
+// void namaste();
+// void bonjour();
+
+// int main(){
+
+
+//     char lang;
+// printf("enter langugae :");
+// scanf("%c",&lang);
+
+
+//     if(lang == 'i'){
+
+//         namaste();
+//     }
+    
+//     else if(lang == 'f'){
+
+
+//         bonjour();
+//     }
+    
+//     else{
+
+//         printf("enter a language again ");
+//     }
+
+//     return 0;
+// }
+
+// void namaste(){
+
+
+//     printf("namaste");
+// }
+
+// void bonjour(){
+
+//     printf("bonjour");
+
+// }
+
+
+#include<stdio.h>
+
+
+
+
+void namaste();
+void bonjour();
+
+int main(){
+
+
+    namaste();
+
+    return 0;
+}
+
+void namaste(){
+
+
+    printf("namaste\n");
+    bonjour();
+}
+
+void bonjour(){
+
+    printf("bonjour");
+
+}
