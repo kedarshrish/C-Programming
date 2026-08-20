6f600e19b11e49864eee51c870247ca59d9672d8
kedarshrish <kedarshrish@gmail.com>
2026-08-20 23:43:13 +0530

    Added 42.c

---
+++ 42.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+
+
+// float squareRoot();
+
+// int main(){
+
+//     printf("the square root is %f", squareRoot());
+
+
+// }
+
+// float squareRoot(){
+//     float x ;
+//     printf("enter x :");
+//     scanf("%F",&x);
+
+//     float sq = sqrt(x);
+//     return sq;
+
+// }
+
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    float n;
+    printf("enter n :");
+    scanf("%f",&n);
+
+    printf("%f",pow(n,0.5));
+
+}
