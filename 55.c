9bd8e6a1847a6de0e36feda03fdd8550cd5d3bcc
kedarshrish <kedarshrish@gmail.com>
2026-08-22 21:59:48 +0530

    Added 55.c

---
+++ 55.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+  int marks[2][3];
+
+ marks[0][0] = 98;
+ marks[0][1] = 97;
+ marks[0][2] = 96;
+
+
+ marks[1][0] = 94;
+ marks[1][1] = 95;
+ marks[1][2] = 93;
+
+printf("%d\t", marks[0][0]);
+printf("%d\t", marks[1][0]);
+printf("%d\t", marks[1][1]);
+
+
+     
+
+
+}
