98c12183ef4d2f48d52fe93b0806b6396852704a
kedarshrish <kedarshrish@gmail.com>
2026-08-24 22:17:46 +0530

    Added 75.c

---
+++ 75.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+char firststr[] = "Apple";
+char secStr[] = "Applz";
+
+int lenght = strcmp(firststr, secStr);
+
+printf("%d", lenght);
+
+
+
+
+
+}
