e671c6e8053fa66edd6ffef88fb13e501d3b1301
kedarshrish <kedarshrish@gmail.com>
2026-08-29 22:52:44 +0530

    Added 101.c

---
+++ 101.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+FILE *fptr;
+
+fptr = fopen("hello.txt" , "r");
+
+char ch;
+ch = fgetc(fptr);
+
+while(ch != EOF){
+
+    printf("%c",ch);
+    ch = fgetc(fptr);
+
+}
+
+printf("\n");
+
+}
