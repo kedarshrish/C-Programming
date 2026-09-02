5b1db61154fbf8257090ed0cab53ef0f92233e30
kedarshrish <kedarshrish@gmail.com>
2026-09-02 12:24:11 +0530

    Added 113.c

---
+++ 113.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+
+printf("Enetr n:");
+scanf("%d",&n);
+
+
+int i;
+int sum = 0;
+
+for(i = 1 ; i <= n ; i++){
+
+    sum = sum + i;
+
+
+
+}
+
+printf("%d",sum);
+
+}
